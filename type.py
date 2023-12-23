545d924341b3cd1de2041a62e68124703b00a4c6
ptlx <ptlx@outlook.jp>
2023-12-23 22:45:57 +0900

    creatorのテストを追加

--- type.py
+++ type.py
@@ -1,4 +1,6 @@
 import unittest
+import numpy as np
+
 class Variable:
     def __init__(self, data):
         self.data = data
@@ -24,7 +26,6 @@ class Function:
 
 class TestFunctionMethods(unittest.TestCase):
     def test_manual_back_propagation(self):
-        import numpy as np
         class Square(Function):
             def forward(self, x):
                 y = x ** 2
@@ -56,6 +57,37 @@ class TestFunctionMethods(unittest.TestCase):
         x.grad = A.backward(a.grad)
         # 3.29744...
         self.assertTrue(abs(x.grad-3.29) <= 0.01)
-
+    def test_variable_creator(self):
+        class Square(Function):
+            def forward(self, x):
+                y = x ** 2
+                return y
+            def backward(self, gy):
+                x = self.input.data
+                gx = 2 * x * gy
+                return gx
+        class Exp(Function):
+            def forward(self, x):
+                y = np.exp(x)
+                return y
+            def backward(self, gy):
+                x = self.input.data
+                gx = np.exp(x) * gy
+                return gx
+        """forward"""
+        x = Variable(0.5)
+        A = Square()
+        B = Exp()
+        C = Square()
+        a = A(x)
+        b = B(a)
+        y = C(b)
+        """test"""
+        self.assertTrue(y.creator == C)
+        self.assertTrue(y.creator.input == b)
+        self.assertTrue(y.creator.input.creator == B)
+        self.assertTrue(y.creator.input.creator.input == a)
+        self.assertTrue(y.creator.input.creator.input.creator == A)
+        self.assertTrue(y.creator.input.creator.input.creator.input == x)
 if __name__ == "__main__":
     unittest.main()
